71c3b9dc1028ed16f171ea793f50ca31d2cdfb89
loc.nguyen <nguyenhophuocloc.cb@gmail.com>
2025-04-12 14:30:23 +0700

    refactor | upgrade to version 15

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Script from "next/script";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Script from 'next/script';
 
 const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "Nguyen Ho Phuoc Loc",
-  description: "Nguyen Ho Phuoc Loc Portfolio",
+  title: 'Nguyen Ho Phuoc Loc',
+  description: 'Nguyen Ho Phuoc Loc Portfolio',
   openGraph: {
-    title: "Nguyen Ho Phuoc Loc",
-    description: "Nguyen Ho Phuoc Loc Portfolio",
-    type: "website",
-    images: ["/images/meta.jpg"],
+    title: 'Nguyen Ho Phuoc Loc',
+    description: 'Nguyen Ho Phuoc Loc Portfolio',
+    type: 'website',
+    images: ['/images/meta.jpg'],
   },
 };
 
 const jsonLdData = {
-  "@context": "https://schema.org",
-  "@type": "Person",
-  name: "Nguyen Ho Phuoc Loc",
-  jobTitle: "Web Developer",
-  url: "https://nguyenhophuocloc.vercel.app/",
+  '@context': 'https://schema.org',
+  '@type': 'Person',
+  name: 'Nguyen Ho Phuoc Loc',
+  jobTitle: 'Web Developer',
+  url: 'https://nguyenhophuocloc.vercel.app/',
   sameAs: [
-    "https://www.linkedin.com/in/nguyen-ho-phuoc-loc/",
-    "https://github.com/nguyenhophuocloc",
+    'https://www.linkedin.com/in/nguyen-ho-phuoc-loc/',
+    'https://github.com/nguyenhophuocloc',
   ],
 };
 
@@ -43,7 +43,7 @@ export default function RootLayout({
       lang="en"
       className={`${inter.className} scrollbar-thin scrollbar-track-zinc-800 scrollbar-thumb-zinc-500`}
     >
-      <body>
+      <body suppressHydrationWarning>
         <Header />
         {children}
         <Footer />
