0931066b49ad22d3ff62894bf0f4cd6ac2cca127
loc.nguyen <nguyenhophuocloc.cb@gmail.com>
2025-07-21 23:20:54 +0700

    bugfix | update robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://nguyenhophuocloc.vercel.app/",
+    sitemap: "https://nguyenhophuocloc.vercel.app/sitemap.xml",
   };
 }
