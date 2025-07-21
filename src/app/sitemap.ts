2da46aad4849e89b47041d96e37804aab7f949dd
loc.nguyen <nguyenhophuocloc.cb@gmail.com>
2025-07-21 23:17:09 +0700

    bugfix | clean sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,29 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
-    {
-      url: "https://nguyenhophuocloc.vercel.app/#home",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://nguyenhophuocloc.vercel.app/#experience",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://nguyenhophuocloc.vercel.app/#education",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
-    },
-    {
-      url: "https://nguyenhophuocloc.vercel.app/#project",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.5,
-    },
   ];
 }
